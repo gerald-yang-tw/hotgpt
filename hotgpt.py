49ef52ea9a0461cc647f6e44022e7eb9650ce542
gerald-yang-tw <gerald.yang@canonical.com>
2023-08-14 08:57:04 +0000

    Adjust web interface

--- hotgpt.py
+++ hotgpt.py
@@ -3,14 +3,15 @@ import tarfile
 import os
 import subprocess
 import yaml
+import time
 from keys import OPENAI_API_KEY
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
-llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0.2, max_tokens=2000)
-KERNEL_TEMPLATE = """You are a kernel expert. Given the kernel calltraces, your job is to go through the calltraces and explain every line in detail
-and then analyze the calltraces, list steps how to debug and possible solutions in bullet format.
+llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0.5, max_tokens=2000)
+KERNEL_TEMPLATE = """You are a kernel expert. Given the kernel calltraces, your job is to go through the calltraces, print each calltrace in bullet format and add a detail explanation in different words to the tail of each calltrace
+and then analyze the calltraces and list possible debug steps, and then list all possible solutions in bullet format.
         Calltraces: {text}
         Answer:
 """
@@ -19,8 +20,10 @@ UPLOAD_FOLDER = 'upload'
 EXTRACT_FOLDER = 'extract'
 SUMMARY_FOLDER = 'summary'
 
-if 'output_text' not in st.session_state:
-        st.session_state['output_text'] = ""
+if 'result_text' not in st.session_state:
+        st.session_state['result_text'] = ""
+if 'log_text' not in st.session_state:
+        st.session_state['log_text'] = ""
 
 st.set_page_config(page_title="HotGPT demo", page_icon=":robot:", layout="wide")
 
@@ -29,21 +32,32 @@ def button_hotsos():
     with open(summary_file, 'r') as file:
         result = yaml.safe_load(file)
         output = str(result['kernel']['potential-issues']['KernelErrors']).lstrip('[\'').rstrip('\']').split('LLM_PROMPT')[0]
-        st.session_state['output_text'] = output
+        calltrace = str(result['kernel']['potential-issues']['KernelErrors']).lstrip('[\'').rstrip('\']').split('LLM_PROMPT')[1].rstrip('(origin=kernel.auto_scenario_check)')
+        calltrace = calltrace.replace('\\n', '\n')
+        st.session_state['result_text'] = output
+        st.session_state['log_text'] = calltrace
 
 def button_hotgpt():
     summary_file = st.session_state['summary_file']
-    with open(summary_file, 'r') as file:
-        result = yaml.safe_load(file)
-        calltrace = str(result['kernel']['potential-issues']['KernelErrors']).lstrip('[\'').rstrip('\']').split('LLM_PROMPT')[1].rstrip('(origin=kernel.auto_scenario_check)')
-        st.session_state['output_text'] = 'thinking...'
-        prompt_template = PromptTemplate(input_variables=["text"], template=KERNEL_TEMPLATE)
-        answer_chain = LLMChain(llm=llm, prompt=prompt_template)
-        answer = answer_chain.run(calltrace)
-        st.session_state['output_text'] = answer
+    llm_file = summary_file + '.txt'
+    if not os.path.exists(llm_file):
+        with open(summary_file, 'r') as file:
+            result = yaml.safe_load(file)
+            calltrace = str(result['kernel']['potential-issues']['KernelErrors']).lstrip('[\'').rstrip('\']').split('LLM_PROMPT')[1].rstrip('(origin=kernel.auto_scenario_check)')
+            calltrace = calltrace.replace('\\n', '\n')
+            prompt_template = PromptTemplate(input_variables=["text"], template=KERNEL_TEMPLATE)
+            answer_chain = LLMChain(llm=llm, prompt=prompt_template)
+            answer = answer_chain.run(calltrace)
+            st.session_state['result_text'] = answer
+            st.session_state['log_text'] = calltrace
+            with open(llm_file, 'w') as sfile:
+                sfile.write(answer)
+    else:
+        with open(llm_file, 'r') as lfile:
+            answer = lfile.read()
+            st.session_state['result_text'] = answer
 
-
-col1, col2, col3 = st.columns((3,3,3))
+col1, col2, col3 = st.columns((3,1,5))
 with col1:
     uploaded_file = st.file_uploader(label="Upload sosreport", type="tar.xz")
     if uploaded_file is not None:
@@ -69,23 +83,24 @@ with col1:
 
         if not os.path.exists(sosreport_path):
             with open(sosreport_path, "wb") as f:
-                print('upload sosreport')
                 f.write(uploaded_file.getvalue())
 
         if not os.path.exists(extract_path):
-            print('extract sosreport')
             tar = tarfile.open(sosreport_path, "r:xz")
             tar.extractall(EXTRACT_FOLDER)
 
         if not os.path.exists(summary_file):
-            print('run hotsos')
             subprocess.call("hotsos --kernel --short -s --output-path " + summary_path + " " + extract_path, shell=True)
+            st.session_state['status_text'] = 'done'
 
 btn1, btn2, btn3 = st.columns((3,3,8))
 with btn1:
     st.button('Ask HotSOS', on_click = button_hotsos)
-
 with btn2:
-    st.button('Ask HotGPT', on_click = button_hotgpt)
+    st.button('Ask LLM', on_click = button_hotgpt)
 
-st.text_area("", height = 500, key = 'output_text')
+result1, result2 = st.columns((3,4))
+with result1:
+    st.text_area("Error logs found in uploaded sosreport", label_visibility = "visible", height = 500, key = 'log_text')
+with result2:
+    st.text_area("Analysis ouput", label_visibility = "visible", height = 500, key = 'result_text')
